d316878682999f64724b28f816d259bc73c609cb
plcr9 <paulcooperpc9@gmail.com>
2023-08-07 15:03:33 +0100

    Second mock test added

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -6,6 +6,11 @@ def test_diary_counts_words_in_all_entries():
     diary.add(FakeTwoWordDiaryEntry())
     assert diary.count_words() == 6
 
+def test_diary_entry_counts_words_in_a_single_entry():
+    diary = Diary()
+    diary.add(FakeFourWordDiaryEntry())
+    assert diary.count_words() == 4
+
 class FakeFourWordDiaryEntry():
     def count_words(self):
         return 4
