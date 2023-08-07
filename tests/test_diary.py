bcb9f70f73d25d6eadeff75ff717daa55bfe01e5
plcr9 <paulcooperpc9@gmail.com>
2023-08-07 15:09:51 +0100

    Refactor mock test using import mock method

--- tests/test_diary.py
+++ tests/test_diary.py
@@ -1,20 +1,19 @@
+from unittest.mock import Mock
 from lib.diary import Diary
 
 def test_diary_counts_words_in_all_entries():
     diary = Diary()
-    diary.add(FakeFourWordDiaryEntry())
-    diary.add(FakeTwoWordDiaryEntry())
+    four_word_mock = Mock()
+    four_word_mock.count_words.return_value = 4
+    diary.add(four_word_mock)
+    two_word_mock = Mock()
+    two_word_mock.count_words.return_value = 2
+    diary.add(two_word_mock)
     assert diary.count_words() == 6
 
 def test_diary_entry_counts_words_in_a_single_entry():
     diary = Diary()
-    diary.add(FakeFourWordDiaryEntry())
+    four_word_mock = Mock()
+    four_word_mock.count_words.return_value = 4
+    diary.add(four_word_mock)
     assert diary.count_words() == 4
-
-class FakeFourWordDiaryEntry():
-    def count_words(self):
-        return 4
-    
-class FakeTwoWordDiaryEntry():
-    def count_words(self):
-        return 2
